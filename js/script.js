b5c5a97717de7f64899dcc8bbb383ab216db1a19
Avers1o <atlant1696@mail.ru>
2022-09-22 23:35:42 +0300

    Add hide and show

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none'; // inline-стили.
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block'; // inline-стили.
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+});
